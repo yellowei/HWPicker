e2842d2309c937603ce8489d5f8f02407670a13c
yellowei <yellowei@yelloweis-MacBook-Pro.local>
2017-05-29 18:39:12 +0800

    解耦
    删除SDWebImage

--- HWPicker/ViewController.swift
+++ HWPicker/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SDWebImage
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PhotoPickerControllerDelegate {
 
--- Podfile
+++ Podfile
@@ -3,6 +3,6 @@ platform:ios,8.0
 use_frameworks!
 
 target ‘HWPicker’ do
-    pod 'SDWebImage','3.8.1'
+#    pod 'SDWebImage','3.8.1'
     pod 'MBProgressHUD'
 end
